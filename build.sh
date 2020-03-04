e0ca1314e6cd4c1836bd80eb230156e137c40c0e
Andrew Top <aabtop@gmail.com>
2020-03-03 17:52:59 -0800

    Remove "-i" from docker run command.

--- build.sh
+++ build.sh
@@ -46,7 +46,10 @@ done
 # Make sure the Docker container image containing the build environment is
 # up to date and then run the actual build command inside the container.
 ${DOCKER_COMMAND} build -q -t test_build ${DOCKERFILE_DIRECTORY} && \
-${DOCKER_COMMAND} run --rm --name test_run -it \
+${DOCKER_COMMAND} run \
+    --rm \
+    --name test_run \
+    -t \
     --mount type=bind,source=${HOST_SRC_DIR},target=/build/src,readonly \
     --mount type=bind,source=${HOST_OUT_DIR},target=/build/out \
     test_build \
