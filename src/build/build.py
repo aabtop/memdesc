adf439c969b5bb02b202051a96e9421b910bd05e
Andrew Top <aabtop@gmail.com>
2020-03-04 23:11:30 -0800

    Run tests as part of the GitHub Actions workflow.

--- src/build/build.py
+++ src/build/build.py
@@ -11,4 +11,7 @@ src_folder = os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir
 
 os.chdir(args.out_folder)
 subprocess.run(['cmake', src_folder]).check_returncode()
-subprocess.run(['cmake', '--build', '.']).check_returncode()  # [..., '--', '-j', str(os.cpu_count())]
+subprocess.run(['cmake', '--build', '.']).check_returncode()
+
+# And finally run the tests before we call it a day.
+subprocess.run(['ctest']).check_returncode()
