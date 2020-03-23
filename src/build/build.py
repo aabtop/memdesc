bf2b773e9673ecb2ac473ba178d10d16068bdf7f
Andrew Top <aabtop@gmail.com>
2020-03-23 00:09:24 -0700

    Add support for running clang-tidy in the Docker build.

--- src/build/build.py
+++ src/build/build.py
@@ -1,5 +1,7 @@
 import argparse
+import json
 import os
+import shutil
 import subprocess
 
 parser = argparse.ArgumentParser(description='Run a build.')
@@ -10,8 +12,25 @@ args = parser.parse_args()
 src_folder = os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir)
 
 os.chdir(args.out_folder)
-subprocess.run(['cmake', src_folder]).check_returncode()
+subprocess.run(['cmake', '-DCMAKE_EXPORT_COMPILE_COMMANDS=ON',
+                src_folder]).check_returncode()
 subprocess.run(['cmake', '--build', '.']).check_returncode()
 
-# And finally run the tests before we call it a day.
+# Run the tests.
 subprocess.run(['ctest', '--output-on-failure']).check_returncode()
+
+# Run clang-tidy, if it's available.
+if shutil.which('run-clang-tidy'):
+  files_in_build = []
+  with open('compile_commands.json') as compile_commands_file:
+    files_in_build = [x['file'] for x in json.load(compile_commands_file)]
+
+  files_to_scan = [x for x in files_in_build if not (
+    ('parser_wrapper' in x) or
+    ('lexer_wrapper' in x) or
+    ('third_party' in x))]
+
+  print(str(files_to_scan))
+
+  # Run the linter.
+  subprocess.run(['run-clang-tidy'] + files_to_scan).check_returncode()
